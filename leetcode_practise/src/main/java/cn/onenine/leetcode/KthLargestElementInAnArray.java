522b2b2bd9bd6e7b01b9d80a81218fa10f5ccde1
lhj502819 <54894887+lhj502819@users.noreply.github.com>
2023-02-02 23:44:52 +0800

    代码提交

---
+++ leetcode_practise/src/main/java/cn/onenine/leetcode/KthLargestElementInAnArray.java
@@ -0,0 +1,35 @@
+package cn.onenine.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Description：数组中的第K个最大元素 leetcode 215题
+ *
+ * @author li.hongjian
+ * @email lhj502819@163.com
+ * @since 2023/2/2 22:36
+ */
+public class KthLargestElementInAnArray {
+
+    /**
+     * 给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。
+     *
+     * 输入: [3,2,1,5,6,4], k = 2
+     * 输出: 5
+     *
+     * 通过快速排序的分而治之的方式
+     */
+    public int findKthLargest(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k <= 0 || k > nums.length) {
+            return 0;
+        }
+
+        //目标元素
+        int target = nums.length - k;
+        
+
+    }
+
+
+}
---
+++ leetcode_practise/src/main/java/cn/onenine/leetcode/QuickSort.java
@@ -0,0 +1,56 @@
+package cn.onenine.leetcode;
+
+/**
+ * Description：快速排序算法
+ *
+ * @author li.hongjian
+ * @email lhj502819@163.com
+ * @since 2023/2/2 23:27
+ */
+public class QuickSort {
+
+    public int[] sortArray(int[] array) {
+
+        quickSort(array, 0, array.length - 1);
+        return array;
+    }
+
+    public void quickSort(int[] nums, int left, int right) {
+        int pivotIndex = partition(nums, left, right);
+        //左分区排序
+        quickSort(nums, left, pivotIndex - 1);
+        //右分区排序
+        quickSort(nums, pivotIndex + 1, right);
+    }
+
+    /**
+     * 分区排序
+     *
+     * @param left  左边界
+     * @param right 有边界
+     */
+    private int partition(int[] nums, int left, int right) {
+        //获取到左边界的值
+        int pivot = nums[left];
+        int j = left;
+        for (int i = left + 1; i < right; i++) {
+            //当前元素小于分区元素，则把当前元素与右分区的最左边
+            if (nums[i] <= pivot) {
+                j++;
+                swap(nums, i, j);
+            }
+        }
+
+        //最终把分区元素放到左分区的最右边，也就是当前元素正确的位置
+        swap(nums, left, j);
+        //分区元素在整个数组中按照升序排序的位置
+        return j;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int old = nums[i];
+        nums[i] = nums[j];
+        nums[j] = old;
+    }
+
+}
