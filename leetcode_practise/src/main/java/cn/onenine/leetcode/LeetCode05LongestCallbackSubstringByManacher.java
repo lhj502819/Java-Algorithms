62ddb7ff3e74183a62b02a2c6233b3b26837dcc4
lhj502819 <54894887+lhj502819@users.noreply.github.com>
2023-02-19 18:36:33 +0800

    通过Manacher算法解决最长回文子串

---
+++ leetcode_practise/src/main/java/cn/onenine/leetcode/LeetCode05LongestCallbackSubstringByManacher.java
@@ -0,0 +1,79 @@
+package cn.onenine.leetcode;
+
+/**
+ * Description：最长回文子串，leetcode第5题 使用Manacher算法
+ *
+ * @author li.hongjian
+ * @email lhj502819@163.com
+ * @since 2023/2/19 13:47
+ */
+public class LeetCode05LongestCallbackSubstringByManacher {
+
+    public static String  manacher(String s) {
+        if (s == null || s.length() == 0) {
+            return s;
+        }
+        //aabbcc =>> #a#a#b#b#c#c#
+        char[] strC = manacherString(s);
+        //存储每个元素的最长回文半径
+        int[] arr = new int[strC.length];
+        //当前最长回文右边界 +1
+        int R = -1;
+        //当前回文中心坐标
+        int C = -1;
+        //最大回文子串的中心坐标
+        int mid = 0;
+        //最长回文子串的长度
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < strC.length; i++) {
+            //确定当前i为中心的最小回文半径是多大，然后再继续往外扩
+            //当R<=i 最小回文半径至少是它本身，也就是1
+            //当R>i时，分三种情况
+            //  ①: i'的L和R在 当前L、R范围内，则i的最大回文半径是i'的回文半径，小于R-i
+            //  ②: i'的L在 超出当前的L，则i的最大回文半径是R-i
+            //  ③: i'的L与当前的L压线（相等），则i的回文半径，至少是R-i
+            // 综上三种情况，则回文半径只有两种情况，i'的回文半径 和 R-i取最小值，最小值则是我们想要的结果
+            //针对第①、②种情况， 都不需要再继续向两侧扩，因为已经能确定最大回文半径了
+            //只有针对第③种情况，才不确定是否已经是最大回文半径，需要继续扩，因此在下方需要进行判断
+            arr[i] = R > i ? Math.min(arr[C * 2 - i], R - i) : 1;
+
+            //确定完最小回文，进行两侧扩容比对
+            //如果以i为中心，加上和减去最小回文半径不超过当前的字符串的两端，则继续扩
+            while (i + arr[i] < strC.length && i - arr[i] > -1) {
+                if (strC[i + arr[i]] == strC[i - arr[i]]) {
+                    arr[i] = arr[i] + 1;
+                } else {
+                    break;
+                }
+            }
+
+            if (i + arr[i] > R) {
+                R = i + arr[i];
+                C = i;
+            }
+            if (max < arr[i]){
+                mid = i;
+                max = arr[i];
+            }
+        }
+
+        //#a#b#b#a#
+        //C = 4
+//        return (max * 2 - 1) / 2;
+        //(max * 2 - 1) / 2 等价于 max - 1
+        return new String(strC).substring(mid - arr[mid] + 1,mid + arr[mid]).replace("#","");
+    }
+
+    public static char[] manacherString(String str) {
+        String result = "#";
+        for (int i = 0; i < str.length(); i++) {
+            result += str.charAt(i) + "#";
+        }
+        return result.toCharArray();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(manacher("babad"));
+    }
+
+}
---
+++ leetcode_practise/src/main/java/cn/onenine/leetcode/ReviewPractise.java
@@ -0,0 +1,91 @@
+package cn.onenine.leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Description：题目回顾
+ *
+ * @author li.hongjian
+ * @email lhj502819@163.com
+ * @since 2023/2/19 9:37
+ */
+public class ReviewPractise {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        if (nums == null || nums.length == 0) {
+            return new int[0];
+        }
+
+        Map<Integer, Integer> map = new HashMap();
+
+        for (int i = 0; i < nums.length; i++) {
+            int nd = target - nums[i];
+            if (map.containsKey(nd)) {
+                return new int[]{map.get(nd), i};
+            }
+            map.put(nums[i], i);
+        }
+
+        return new int[0];
+    }
+
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+
+        //进位
+        int carry = 0;
+        ListNode head = null;
+        ListNode prev = null;
+        ListNode c1 = l1;
+        ListNode c2 = l2;
+
+        while (c1 != null || c2 != null) {
+            int v1 = c1 != null ? c1.val : 0;
+            int v2 = c2 != null ? c2.val : 0;
+            //求和
+            int sum = v1 + v2 + carry;
+            //进位，对10取余
+            carry = sum / 10;
+            //当前值
+            int current = sum % 10;
+            ListNode nowNode = new ListNode(current);
+            if (head == null) {
+                head = nowNode;
+            } else {
+                prev.next = nowNode;
+            }
+            prev = nowNode;
+            c1 = c1 != null ? c1.next : null;
+            c2 = c2 != null ? c2.next : null;
+        }
+
+        if (carry != 0) {
+            prev.next = new ListNode(carry);
+        }
+
+        return head;
+    }
+
+
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0)
+            return 0;
+
+        int[] arr = new int[256];
+        Arrays.fill(arr, -1);
+        int maxLength = 0;
+        int prev = -1;
+        for (int i = 0; i < s.length(); i++) {
+            prev = Math.max(arr[s.charAt(i)], prev);
+            maxLength = Math.max(maxLength, i - prev);
+            arr[s.charAt(i)] = i;
+        }
+
+        return maxLength;
+    }
+}
+
